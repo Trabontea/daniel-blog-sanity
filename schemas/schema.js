1ca3f88f715a4d618e5f4a263f6f28452ad4134b
Daniel Botea <danielmbotea@gmail.com>
2021-04-15 01:21:55 +0300

    add blog to fe

--- schemas/schema.js
+++ schemas/schema.js
@@ -12,5 +12,27 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    {
+      name: 'blog',
+      type: 'document',
+      title: 'BLOG',
+      fields:[
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title'
+        },
+        {
+          name: 'subtitle',
+          type: 'string',
+          title: 'Subtitle'
+        },
+        {
+          name:'slug',
+          type: 'slug',
+          title: 'Slug'
+        }
+      ]
+    }
   ]),
 })
