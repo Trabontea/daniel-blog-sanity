7995a5a9a8e4024ca981c8afb408073f799a5029
Daniel Botea <danielmbotea@gmail.com>
2021-12-07 09:44:41 +0200

    update build

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,202 +1,180 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
     {
-      name: 'author',
-      type: 'document',
-      title: 'Author',
-      fields:[
-        {
-          name: 'name',
-          title: 'Name',
-          type: 'string'
-        },
-        {
-          name: 'avatar',
-          title: 'Avatar',
-          type: 'image'
-        }
-      ]
-    },
-    {
-      name: 'category',
-      type: 'document',
-      title: 'Category',
+      name: "author",
+      type: "document",
+      title: "Author",
       fields: [
         {
-          name: 'name',
-          type: 'string',
-          title: 'Name',
+          name: "name",
+          title: "Name",
+          type: "string",
         },
         {
-          name:'slug',
-          type: 'slug',
-          title: 'Slug',
-          validation: Rule => Rule.required()
-        }
-      ]
+          name: "avatar",
+          title: "Avatar",
+          type: "image",
+        },
+      ],
     },
-      /* BLOG */
     {
-      name: 'blog',
-      type: 'object',
-      title: 'BLOG',
-      fields:[
+      name: "category",
+      type: "document",
+      title: "Category",
+      fields: [
         {
-          name: 'title',
-          type: 'string',
-          title: 'Title',
-          validation: (Rule)=> {return Rule.required().min(3)}
+          name: "name",
+          type: "string",
+          title: "Name",
         },
         {
-          name: 'subtitle',
-          type: 'string',
-          title: 'Subtitle'
+          name: "slug",
+          type: "slug",
+          title: "Slug",
+          validation: (Rule) => Rule.required(),
+        },
+      ],
+    },
+    {
+      name: "blog",
+      type: "object",
+      title: "BLOG",
+      fields: [
+        {
+          name: "title",
+          type: "string",
+          title: "Title",
+          validation: (Rule) => {
+            return Rule.required().min(3);
+          },
         },
         {
-          name: 'category',
-          type: 'reference',
-          title: 'Category',
-          to:[{type:'category'}],
+          name: "subtitle",
+          type: "string",
+          title: "Subtitle",
+        },
+        {
+          name: "category",
+          type: "reference",
+          title: "Category",
+          to: [{ type: "category" }],
           // options: {
           //   filter: 'category == $category',
           //   filterParams: {category: 'category'}
           // },
-          validation: Rule => Rule.required()
+          validation: (Rule) => Rule.required(),
         },
         {
-          name: 'year',
-          type: 'string',
-          title: 'Year'
+          name: "year",
+          type: "string",
+          title: "Year",
         },
         {
-          name: 'coverImage',
-          title: 'Cover Image',
-          type:'image',
+          name: "coverImage",
+          title: "Cover Image",
+          type: "image",
           options: {
-            hotspot: true
+            hotspot: true,
           },
           fields: [
             {
-              type: 'text',
-              name: 'alt',
-              title: 'Description'
-            }
-          ]
+              type: "text",
+              name: "alt",
+              title: "Description",
+            },
+          ],
         },
         {
-          name:'content',
-          title:'Content',
-          type: 'array',
+          name: "content",
+          title: "Content",
+          type: "array",
           of: [
             {
-              type: 'block'
+              type: "block",
             },
             {
-              type: 'image',
+              type: "image",
               fields: [
                 {
-                  type: 'text',
-                  name: 'alt',
-                  title: 'Description',
+                  type: "text",
+                  name: "alt",
+                  title: "Description",
                   options: {
-                    isHighlighted: true
-                  }
+                    isHighlighted: true,
+                  },
                 },
                 {
-                  title: 'Image Position',
-                  name: 'position',
-                  type: 'string',
+                  title: "Image Position",
+                  name: "position",
+                  type: "string",
                   options: {
                     list: [
-                      {title: 'Left', value: 'left'},
-                      {title: 'Center', value: 'center'},
-                      {title: 'Right', value: 'right'},
+                      { title: "Left", value: "left" },
+                      { title: "Center", value: "center" },
+                      { title: "Right", value: "right" },
                     ],
-                    layout: 'radio',
-                    isHighlighted: true
-                  }
-                }
+                    layout: "radio",
+                    isHighlighted: true,
+                  },
+                },
               ],
               options: {
-                hotspot: true
-              }
+                hotspot: true,
+              },
             },
             {
-              type: 'code',
-              name: 'code',
+              type: "code",
+              name: "code",
               options: {
-                withFilename: true
-              }
+                withFilename: true,
+              },
             },
-          ]
+          ],
         },
         {
-          name: 'date',
-          title: 'Date',
-          type: 'datetime',
-          validation: Rule => Rule.required()
+          name: "date",
+          title: "Date",
+          type: "datetime",
+          validation: (Rule) => Rule.required(),
         },
         {
-          name: 'author',
-          title: 'Author',
-          type: 'reference',
-          to:[{type:'author'}],
-          validation: Rule => Rule.required()
+          name: "author",
+          title: "Author",
+          type: "reference",
+          to: [{ type: "author" }],
+          validation: (Rule) => Rule.required(),
         },
         {
-          name:'slug',
-          type: 'slug',
-          title: 'Slug',
-          validation: Rule => Rule.required()
-        }
-      ]
+          name: "slug",
+          type: "slug",
+          title: "Slug",
+          validation: (Rule) => Rule.required(),
+        },
+      ],
     },
-    // {
-    //   name: 'album',
-    //   type: 'document',
-    //   title: 'Album',
-    //   fields: [
-    //     {
-    //       name: 'band',
-    //       type: 'string',
-    //       title: 'Band'
-    //     },
-    //     {
-    //       name: 'album',
-    //       type: 'string',
-    //       title: 'Album'
-    //     },
-    //     {
-    //       name: 'year',
-    //       type: 'number',
-    //       title: 'Year'
-    //     },
-    //   ]
-    // },
     {
-      name: 'band',
-      type: 'document',
-      title: 'Band',
+      name: "band",
+      type: "document",
+      title: "Band",
       fields: [
         {
-          name: 'name',
-          type: 'string',
-          title: 'Name'
-        }
-      ]
+          name: "name",
+          type: "string",
+          title: "Name",
+        },
+      ],
     },
-
   ]),
-})
+});
